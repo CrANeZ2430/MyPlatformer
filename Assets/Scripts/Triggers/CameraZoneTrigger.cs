ad581efd051dec695b05e1cea5464ab15c90251f
CrANeZ2430 <crn.alexei@gmail.com>
2026-05-31 19:05:38 +0200

    fixed bugs

--- Assets/Scripts/Triggers/CameraZoneTrigger.cs
+++ Assets/Scripts/Triggers/CameraZoneTrigger.cs
@@ -29,7 +29,7 @@ public class CameraZoneTrigger : MonoBehaviour
         anchorMax.transform.SetParent(transform);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag(playerTag))
         {
